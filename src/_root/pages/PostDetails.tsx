5aaee3168335e7b83b0f2cfec2ced9052809a737
MustafaAyman <mostst124@gmail.com>
2023-12-07 15:23:07 +0200

    fourth  commit

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -66,7 +66,7 @@ const PostDetails = () => {
                   <Button
                     onClick={handleDeletePost}
                     variant= "ghost"
-                    className={` ${user.id !== post?.creator.$id} && "ghost_details-delete_btn hidden"`}
+                    className={` ${user.id !== post?.creator.$id && "ghost_details-delete_btn"}"`}
 
                   >
                     <img
